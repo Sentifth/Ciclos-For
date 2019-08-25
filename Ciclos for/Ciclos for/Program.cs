0960d74b3ebd71aee3dbb0ed1bbe48cde06f0366
Jayro Paúl Guzmán Murguía <jguzman14@ucol.mx>
2019-08-24 21:24:26 -0500

    2.0

--- Ciclos for/Ciclos for/Program.cs
+++ Ciclos for/Ciclos for/Program.cs
@@ -1,45 +1,44 @@
 ﻿using System;
 
-namespace ConsoleApp2
+namespace Ciclos_For
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x, y, z, w;
+            int x, y, z;
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.BackgroundColor = ConsoleColor.White;
             Console.Clear();
-            Console.WriteLine("<<Rango de tu ciclo>>");
+            Console.WriteLine("<<Rango>>");
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("De donde arrancamos el ciclo: ");
+            Console.WriteLine("De donde empezamos el ciclo: ");
             Console.ForegroundColor = ConsoleColor.Black;
             x = Convert.ToInt32(Console.ReadLine());
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("De donde terminamos el ciclo (Menor):");
+            Console.WriteLine("Escribe hasta donde terminamos el ciclo el ciclo:");
             Console.ForegroundColor = ConsoleColor.Black;
             y = Convert.ToInt32(Console.ReadLine());
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("De donde terminamos el ciclo (Mayor):");
-            Console.ForegroundColor = ConsoleColor.Black;
-            w = Convert.ToInt32(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("De cuanto en cuanto: ");
+            Console.WriteLine("De cuanto en cuanto? ");
             Console.ForegroundColor = ConsoleColor.Black;
             z = Convert.ToInt32(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("<<Ciclo Menor>>");
-            for (int i = x; i > y; i -= z)
+
+            if (x < y)
             {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("i = " + i);
+                for (int i = x; i <= y; i += z)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("i = " + i);
+                }
             }
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("<<Ciclo Mayor>>");
-            for (int i = x; i < w; i += z)
+            if (x > y)
             {
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("i = " + i);
+                for (int i = x; i >= y; i -= z)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("i = " + i);
+                }
             }
         }
     }
